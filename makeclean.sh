54a6a3a91be0bb1d44fd47427542d01dc1f05bc1
xulz <xulongzhong2004@163.com>
2020-12-28 10:36:21 +0800

    ...

---
+++ makeclean.sh
@@ -0,0 +1,35 @@
+cd src/util
+pwd
+make clean
+cd ../..
+
+cd src/dbmysql
+pwd
+make clean
+cd ../..
+
+
+cd src/uhp
+pwd
+make clean
+cd ../..
+
+cd src/sdk
+pwd
+make clean
+cd ../..
+
+cd src/plugin/sequence/plugin_input
+pwd
+make clean
+cd ../../../..
+pwd
+
+cd src/plugin/sequence/plugin_output
+pwd
+make clean
+cd ../../../..
+pwd
+
+
+
